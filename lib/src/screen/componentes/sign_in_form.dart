f1916e617610acbb37c1549616b07a0c88d70c0d
Percibal <vasquez.percibal@gmail.com>
2024-01-09 18:22:02 -0300

    padingalsingin

--- lib/src/screen/componentes/sign_in_form.dart
+++ lib/src/screen/componentes/sign_in_form.dart
@@ -3,7 +3,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:rive/rive.dart';
-
 class SignInForm extends StatefulWidget {
   const SignInForm({
     Key? key,
@@ -12,7 +11,6 @@ class SignInForm extends StatefulWidget {
   @override
   State<SignInForm> createState() => _SignInFormState();
 }
-
 class _SignInFormState extends State<SignInForm> {
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   bool isShowLoading = false;
@@ -27,7 +25,6 @@ class _SignInFormState extends State<SignInForm> {
     artboard.addController(controller!);
     return controller;
   }
-
   void signIn(BuildContext context) {
     setState(() {
       isShowLoading = true;
@@ -61,93 +58,91 @@ class _SignInFormState extends State<SignInForm> {
       },
     );
   }
-
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "Email",
-                style: TextStyle(color: Colors.black54),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8, bottom: 16),
-                child: TextFormField(
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "";
-                    }
-                    return null;
-                  },
-                  onSaved: (email) {},
-                  decoration: InputDecoration(
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8),
-                      child: SvgPicture.asset("assets/icons/email.svg"),
-                    ),
-                  ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Email",
+                  style: TextStyle(color: Colors.black54),
                 ),
-              ),
-              const Text(
-                "Password",
-                style: TextStyle(color: Colors.black54),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8, bottom: 16),
-                child: TextFormField(
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "";
-                    }
-                    return null;
-                  },
-                  onSaved: (password) {},
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8),
-                      child: SvgPicture.asset("assets/icons/password.svg"),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 8, bottom: 24),
-                child: ElevatedButton.icon(
-                  onPressed: () {
-                    signIn(context);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFF77D8E),
-                    minimumSize: const Size(double.infinity, 56),
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(25),
-                        bottomRight: Radius.circular(25),
-                        bottomLeft: Radius.circular(25),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8, bottom: 16),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "";
+                      }
+                      return null;
+                    },
+                    onSaved: (email) {},
+                    decoration: InputDecoration(
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        child: SvgPicture.asset("assets/icons/email.svg"),
                       ),
                     ),
                   ),
-                  icon: const Icon(
-                    CupertinoIcons.arrow_right,
-                    color: Color(0xFFFE0037),
-                  ),
-                  label: const Text("Sign In"),
                 ),
-              ),
-            ],
+                const Text(
+                  "Password",
+                  style: TextStyle(color: Colors.black54),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8, bottom: 16),
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "";
+                      }
+                      return null;
+                    },
+                    onSaved: (password) {},
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      prefixIcon: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                        child: SvgPicture.asset("assets/icons/password.svg"),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8, bottom: 24),
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      signIn(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFF77D8E),
+                      minimumSize: const Size(double.infinity, 56),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(25),
+                          bottomRight: Radius.circular(25),
+                          bottomLeft: Radius.circular(25),
+                        ),
+                      ),
+                    ),
+                    icon: const Icon(
+                      CupertinoIcons.arrow_right,
+                      color: Color(0xFFFE0037),
+                    ),
+                    label: const Text("Sign In"),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-        // As you can see there is 3 trigger
-        // Want to show the loading once user tab on the Sign in button
-        // If all okey it should show success
-        // otherwise error
         isShowLoading
             ? CustomPositioned(
                 child: RiveAnimation.asset(
@@ -162,8 +157,6 @@ class _SignInFormState extends State<SignInForm> {
                 ),
               )
             : const SizedBox(),
-
-        // Looks good
         isShowConfetti
             ? CustomPositioned(
                 child: Transform.scale(
@@ -185,4 +178,3 @@ class _SignInFormState extends State<SignInForm> {
     );
   }
 }
-
